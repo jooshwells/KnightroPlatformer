21077404e3ece54aec53f9e3b9cbf6f8f2d4e96c
AndyW1016 <andyw101604@gmail.com>
2024-10-05 16:03:38 -0400

    movement
    no fly jump

--- Assets/Code/movement.cs
+++ Assets/Code/movement.cs
@@ -9,6 +9,7 @@ public class movement : MonoBehaviour
 
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private bool grounded;
    
    private void Awake() {
     body = GetComponent<Rigidbody2D>();
@@ -30,8 +31,16 @@ public class movement : MonoBehaviour
             transform.localScale = newScale;
         }
 
-        if(Input.GetKey(KeyCode.Space)) {
-            body.velocity = new Vector2(body.velocity.x, speed/2);
+        if(Input.GetKey(KeyCode.Space) && grounded) {
+            Jump();
         }
+    }
+    private void Jump() {
+        body.velocity = new Vector2(body.velocity.x, speed/2);
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
